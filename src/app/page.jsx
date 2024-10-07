c9f6e41aef3c9ddb07475bc140609024fd5ced95
Lucas <lucasqueteden@hotmail.com>
2024-10-07 13:59:19 +0200

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -65,18 +65,22 @@ export default function Home() {
   return (
     <main className="p-3 sm:p-7 text-center">
       <h1 className="text-4xl md:text-6xl font-bold mb-12 bg-gradient-to-r from-fuchsia-500 to-indigo-600 bg-clip-text text-transparent">
-        Sorteo Navidad
+        Sorteo Cesta de Navidad Los García
       </h1>
       <div className="flex flex-col w-full gap-6">
         <div className="flex flex-col gap-5 items-center justify-center shadow-sm bg-[#3b3b3b] p-3 sm:p-5 rounded-md">
           <h2 className="text-2xl md:text-3xl font-semibold">
-            Lista de numeros
+            Lista de Números
           </h2>
           <p className="font-semibold text-gray-300">
-            Sorte cesta de navidad. Para participar seleciona primero la casilla del numero elegido,
-            continuacion escribe tu nombre, y una vez completa tu eleccion presiona boton enviar.
-            Solo se puede elegir un numero, si quieres elegir deberas hacer el mismo processo otra vez.
-            En caso de dudas consulta con Sandra.
+            Para participar en el sorteo de la cesta de Navidad de Los García, en primer lugar
+            , selecciona la casilla del número que quieres elegir. A continuación, escribe tu nombre.
+            Finalmente, una vez completada tu elección, presiona el botón &quot;Enviar&quot;.
+
+            Solo es posible seleccionar un número, si quieres comprar más números deberás llevar a cabo
+            el mismo processo de nuevo.
+
+            El precio de cada número es de 2 euros. El importe deberás abonárselo a la Eli a través de bizum.
           </p>
           <div className="flex flex-col sm:flex-row sm:flex-wrap gap-3 items-center justify-center">
             {isLoading ? (
@@ -116,8 +120,8 @@ export default function Home() {
                   </>
                 ) : (
                   <div className="flex flex-col gap-8 min-h-[200px] items-center justify-center text-white">
-                    <h3 className="font-bold text-2xl text-red-400">No hay numeros para selecionar!</h3>
-                    <span>Todos los numeros ya selcionados los encontraras en la siguiente lista</span>
+                    <h3 className="font-bold text-2xl text-red-400">No hay números para seleccionar!</h3>
+                    <span>Todos los numeros ya selccionados los encontraras en la siguiente lista</span>
                   </div>
                 )}
               </>
@@ -136,12 +140,14 @@ export default function Home() {
         </div>
         <div className="flex flex-col gap-5 items-center justify-center shadow-sm bg-[#473844] p-3 sm:p-5 rounded-md">
           <h2 className="text-2xl md:text-3xl font-semibold">
-            Lista Numeros selecionados
+            Lista de los Numeros Seleccionados
           </h2>
           <p className="font-semibold text-gray-300">
-            Esta lista corresponde con los numeros ya selecionados con el correspondiente nombre.
-            el sorteo se realizara el dia 25 de diciembre. Una vez esa fecha ya no se podra realizara
-            mas selecion de numeros.
+            Aquí puedes ver la lista de los números que ya están seleccionados, con su correspondiente
+            nombre/propietario. El plzo límite para poder adquirir números es el 21 de Diciembre de 2024.
+            Pasada esta fecha ya no podrás seleccionar ningún número. Para cualquier consulta sobre el sorteo,
+            ponerse en contacto con Eli o Sandra. ¡Estaremos ecantadas de atenderos!
+            PS: Donde pone el nombre de Eli, también os podéis dirigir con el nombre de pamplinas 😂😂
           </p>
           <div className="flex flex-wrap gap-3 items-center justify-center">
             {isLoading ? (
@@ -166,7 +172,7 @@ export default function Home() {
                 ) : (
                   <div className="flex flex-col gap-8 min-h-[200px] items-center justify-center text-white">
                     <h3 className="font-bold text-2xl text-red-400">No hay numeros selecionados aun!</h3>
-                    <span>Seleciona tu numero en la lista de los numeros para selecionar</span>
+                    <span>Selecciona tu número en la lista de los números para seleccionar</span>
                   </div>
                 )}
               </>
